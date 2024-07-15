681737e5b72f619c76f11680869b5a470a3905b5
Gary Pedretti <GaryPedretti@users.noreply.github.com>
2024-07-15 12:04:37 -0500

    1st attempt at interface

--- MonopolySimulation/Square.cs
+++ MonopolySimulation/Square.cs
@@ -8,5 +8,15 @@ namespace MonopolySimulation
 {
     public class Square
     {
+        int _position;
+        string _name;
+        int _price;
+        int _rent;
+        public Square(int position, string name, int price, int rent) {
+            _position = position;
+            _name = name;
+            _price = price;
+            _rent = rent;
+        }
     }
 }
---
+++ MonopolySimulation/SquareDaoTextFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolySimulation
+{
+    public class SquareDaoTextFile : ISquareDao
+    {
+        public Square CreateSquare(int position)
+        {
+            return CreateSquare(position, "", 0, 0);
+        }
+
+        public Square CreateSquare(int position, string name, int price, int rent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
