2f5ecf4aa935f7d3bf2c59291b7368053d09076c
alex <alexey.agapov@gmail.com>
2024-07-17 12:01:20 -0500

    public void ShouldGet200WhenLandingOnGo test added and fails

---
+++ MonopolySimulation.Tests/WhenLandingOnMoneySquare.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolySimulation.Tests
+{
+    public class WhenLandingOnMoneySquare
+    {
+        private MonopolyGame _game;
+        string[] _tokens;
+        IList<Player> _players;
+        int _numOfPlayers;
+
+        public WhenLandingOnMoneySquare()
+        {
+            _game = new();
+            _tokens = ["dog", "sport car", "iron", "battleship", "top hat", "thimble", "wheelbarrow", "cat"];
+            _numOfPlayers = 8;
+            _game.Setup(_numOfPlayers, _tokens);
+            _game.PlayRounds(1);
+            _players = _game.Players!;
+        }
+
+        [Fact]
+        public void ShouldGet200WhenLandingOnGo()
+        {
+            MonopolyBoard board = new MonopolyBoard();
+            Player player = new Player(board.Squares[37], "BLAH");
+            Die die1 = new LoadedDie(1);
+            Die die2 = new LoadedDie(2);
+            var originalAmount = player.CashAmount;
+            
+            //act
+            player.TakeTurn(die1, die2);
+            var newAmount = player.CashAmount;
+
+            // assert
+            Assert.Equal(newAmount, originalAmount + 200);
+        }
+
+    }
+}
