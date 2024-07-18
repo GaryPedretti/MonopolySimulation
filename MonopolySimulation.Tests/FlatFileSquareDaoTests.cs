2b32ffc6cc90cfb4c3993f852fc7af5a96e11ed9
alex <alexey.agapov@gmail.com>
2024-07-18 11:45:38 -0500

    WhenGettingFirstPostionName_GetsFirstStringInCsv test added and passes

---
+++ MonopolySimulation.Tests/FlatFileSquareDaoTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolySimulation.Tests
+{
+    public class FlatFileSquareDaoTests
+    {
+        const string filename = @"..\..\..\..\chicagoMonopoly.txt";
+
+
+        [Fact]
+        public void WhenGettingFirstPostionName_GetsFirstStringInCsv()
+        {
+            FlatFileSquareDao squareDao = new FlatFileSquareDao(filename);
+
+            string squareName = squareDao.GetSquareName(0);
+
+            Assert.Equal("Go", squareName);
+        }
+
+
+        // when constructing square
+        // should use name at position in file for square name
+
+
+    }
+}
---
+++ MonopolySimulation/FlatFileSquareDao.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MonopolySimulation;
+
+public class FlatFileSquareDao(string filename)
+{
+
+    public string GetSquareName(int position)
+    {
+        var names = File.ReadAllText(filename);
+        var array = names.Split(",");
+        var trimmedArray = array.Select(x => x.Trim().Trim('"')).ToArray();
+        return trimmedArray[position];
+    }
+}
